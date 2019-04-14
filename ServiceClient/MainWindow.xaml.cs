df1ec49687f2eae83bf8c27467c2ea9c63f2b27e
AblyazovDavlat <ablyazovdavlat@gmail.com>
2019-04-14 12:56:03 +0300

    Initial version

---
+++ ServiceClient/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ServiceManager.IServiceManagerCallback
+    {
+        bool isConnected = false;
+        ServiceManager.ServiceManagerClient client;
+        int ID;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceManager.ServiceManagerClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbPCName.Text);
+                tbPCName.IsEnabled = false;
+                btnConnect.Content = "Disconnect";
+                isConnected = true;
+            }
+        }
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbPCName.IsEnabled = true;
+                btnConnect.Content = "Connect";
+                isConnected = false;
+            }
+        }
+
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallBack(string msg)
+        {
+            lbLog.Items.Add(msg);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+    }
+}
---
+++ wpf_manager/IServiceManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wpf_manager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceManager" in both code and config file together.
+    [ServiceContract(CallbackContract =typeof(IServiceManagerCallback))]
+    public interface IServiceManager
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+    
+    public interface IServiceManagerCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallBack(string msg);
+    }
+}
